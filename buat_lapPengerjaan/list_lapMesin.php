2dda109a16c97ccb029afa1fc2d089476f29ff40
saveme1507 <asepsoo55@gmail.com>
2020-05-05 23:05:09 +0700

    commit perbaikan bug histori pm

--- buat_lapPengerjaan/list_lapMesin.php
+++ buat_lapPengerjaan/list_lapMesin.php
@@ -3,7 +3,11 @@ include "../koneksi.php";
 
 $mp_id  = $_GET['mp_id'];
 
-$query = mysqli_query($con, "SELECT header_laporan_mesin.hlm_id,header_laporan_mesin.hlm_tanggal,master_mesin.mm_sn,header_laporan_mesin.hlm_ttd FROM header_laporan_mesin INNER JOIN detail_laporan_mesin ON header_laporan_mesin.hlm_id=detail_laporan_mesin.dlm_id INNER JOIN master_mesin ON master_mesin.mm_id = detail_laporan_mesin.dlm_id_mesin WHERE  header_laporan_mesin.hlm_id_teknisi != 0 AND header_laporan_mesin.hlm_id_perusahaan=" . $mp_id);
+if ($mp_id != 1) {
+    $query = mysqli_query($con, "SELECT header_laporan_mesin.hlm_id,header_laporan_mesin.hlm_tanggal,master_mesin.mm_sn,header_laporan_mesin.hlm_ttd FROM header_laporan_mesin INNER JOIN detail_laporan_mesin ON header_laporan_mesin.hlm_id=detail_laporan_mesin.dlm_id INNER JOIN master_mesin ON master_mesin.mm_id = detail_laporan_mesin.dlm_id_mesin WHERE  header_laporan_mesin.hlm_id_teknisi != 0 AND header_laporan_mesin.hlm_id_perusahaan LIKE'" . $mp_id . "' ORDER BY hlm_id DESC");
+} else {
+    $query = mysqli_query($con, "SELECT header_laporan_mesin.hlm_id,header_laporan_mesin.hlm_tanggal,master_mesin.mm_sn,header_laporan_mesin.hlm_ttd FROM header_laporan_mesin INNER JOIN detail_laporan_mesin ON header_laporan_mesin.hlm_id=detail_laporan_mesin.dlm_id INNER JOIN master_mesin ON master_mesin.mm_id = detail_laporan_mesin.dlm_id_mesin WHERE  header_laporan_mesin.hlm_id_teknisi != 0 ORDER BY hlm_id DESC");
+}
 
 $json = array();
 
--- sparepart/list_perPart.php
+++ sparepart/list_perPart.php
@@ -3,7 +3,12 @@ include "../koneksi.php";
 
 $mp_id  = $_GET['mp_id'];
 
-$query = mysqli_query($con, "SELECT histori_sparepart.hs_id,histori_sparepart.hs_tgl,master_mesin.mm_sn,histori_sparepart.hs_ttd FROM histori_sparepart INNER JOIN master_mesin ON histori_sparepart.hs_id_mesin=master_mesin.mm_id WHERE master_mesin.mm_id_pt=" . $mp_id);
+if ($mp_id != 1) {
+    $query = mysqli_query($con, "SELECT histori_sparepart.hs_id,histori_sparepart.hs_tgl,master_mesin.mm_sn,histori_sparepart.hs_ttd FROM histori_sparepart INNER JOIN master_mesin ON histori_sparepart.hs_id_mesin=master_mesin.mm_id WHERE master_mesin.mm_id_pt LIKE'" . $mp_id . "' ORDER BY hs_id DESC");
+} else {
+    $query = mysqli_query($con, "SELECT histori_sparepart.hs_id,histori_sparepart.hs_tgl,master_mesin.mm_sn,histori_sparepart.hs_ttd FROM histori_sparepart INNER JOIN master_mesin ON histori_sparepart.hs_id_mesin=master_mesin.mm_id ORDER BY hs_id DESC");
+}
+
 
 $json = array();
 
